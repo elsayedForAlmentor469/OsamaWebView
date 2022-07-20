9cf8da4bdf7ba76aeef60c4a2cec9d507b3f4e59
elsayedForAlmentor469 <80629051+elsayedForAlmentor469@users.noreply.github.com>
2022-07-20 15:53:38 +0200

    Add project files.

---
+++ OsamaWebView/OsamaWebView/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using HtmlRender;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OsamaWebView
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ OsamaWebView/OsamaWebView/MainPage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HtmlRender
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Bind();
+        }
+        void Bind()
+        {
+            string str = "<html><head><title>HTML Elements Reference</title></head><body>"
+
+ + "<h1>This is a heading</h1>"
+ + "<p>This is a paragraph.</p>"
+ + "<p>This is a paragraph.</p>"
+ + "<p>This is a paragraph.</p>"
+ + "<p>This is a paragraph.</p>"
+ + "<p>This is a paragraph.</p>"
+ + "<p>This is a paragraph.</p>"
+
+ + "</body>"
+ + "</html>";
+            var htmSrc = new HtmlWebViewSource();
+            htmSrc.Html = str;
+            WbView.Source = htmSrc;
+        }
+        private void Btn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var htmSrc = new HtmlWebViewSource();
+                htmSrc.Html = txt.Text;
+                WbView.Source = htmSrc;
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        private void Sld_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            try
+            {
+                /* var hght = Sld.Value;
+                 WbView.HeightRequest = hght;
+                */
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+        void Resize(bool Max)
+        {
+            try
+            {
+                var hght = WbView.Height;
+                if (Max)
+                {
+                    WbView.HeightRequest = hght + 50;
+                }
+                else
+                {
+                    WbView.HeightRequest = hght - 50;
+
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+        private void btnMinClick(object sender, EventArgs e)
+        {
+            Resize(false);
+
+        }
+
+        private void BtnMxclick(object sender, EventArgs e)
+        {
+            Resize(true);
+        }
+
+        private void BtnSrc_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WbView.Source = txt.Text;
+
+            }
+            catch (Exception Excep)
+            {
+
+            }
+         }
+    }
+}
